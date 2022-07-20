5978dd9b4732a86163bded2e3a398eab370011ec
Shoxrux <shoxrux@mail.ru>
2022-07-20 12:47:16 +0500

    uy ishi js

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var user = prompt('Yoshingizni kiriting'),
+    password = '18'
+
+    if (user <= password) {
+    console.log('Yoshsiz o`qishiz kere');
+}
+var user2 = prompt('Yoshingizni kiriting'),
+    password2 = '50'
+
+    if (user2 <= password2) {
+    console.log('Ishlashiz kere');
+}
+var user3 = prompt('Yoshingizni kiriting'),
+    password3 = '60'
+
+    if (user3 <= password3) {
+    console.log('Yetadi doda pensiyaga chiqing');
+}
+var user4 = prompt('Yoshingizni kiriting'),
+    password4 = '150'
+
+    if (user4 <= password4) {
+    console.log('Gap yo lekin doda :)');
+}
